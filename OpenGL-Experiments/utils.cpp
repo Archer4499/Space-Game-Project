2ca467f73a7a05ba1b94a32082713b58f72efdbb
Derek King <king.dm49@gmail.com>
2018-04-02 03:11:18 +1000

    Added vec2 and another constructor

--- OpenGL-Experiments/utils.cpp
+++ OpenGL-Experiments/utils.cpp
@@ -1,30 +1,47 @@
 #include <cmath>
+#include <vector>
 
 #include "utils.h"
 
 
-struct vec3 {
-  float v[3];
-  vec3() {
-    v[0] = 0.0f;
-    v[1] = 0.0f;
-    v[2] = 0.0f;
-  }
-  vec3(float x, float y, float z) {
-    v[0] = x;
-    v[1] = y;
-    v[2] = z;
-  }
-};
+vec2::vec2() {
+    x = 0.0f;
+    y = 0.0f;
+}
+vec2::vec2(float vert[]) {
+    x = vert[0];
+    y = vert[1];
+}
+vec2::vec2(float ax, float ay) {
+    x = ax;
+    y = ay;
+}
+
+
+vec3::vec3() {
+    x = 0.0f;
+    y = 0.0f;
+    z = 0.0f;
+}
+vec3::vec3(float vert[]) {
+    x = vert[0];
+    y = vert[1];
+    z = vert[2];
+}
+vec3::vec3(float ax, float ay, float az) {
+    x = ax;
+    y = ay;
+    z = az;
+}
 
 
 void normalizeVector(vec3 *v) {
-  float len2 = v->v[0] * v->v[0] + v->v[1] * v->v[1] + v->v[2] * v->v[2];
+  float len2 = v->x * v->x + v->y * v->y + v->z * v->z;
   if (len2 > 0.0f) {
     float len = sqrtf(len2);
 
-    v->v[0] /= len;
-    v->v[1] /= len;
-    v->v[2] /= len;
+    v->x /= len;
+    v->y /= len;
+    v->z /= len;
   }
 }
--- OpenGL-Experiments/utils.h
+++ OpenGL-Experiments/utils.h
@@ -1,5 +1,22 @@
 #pragma once
 
-struct vec3;
+struct vec2 {
+  float x;
+  float y;
+
+  vec2();
+  explicit vec2(float vert[]);
+  vec2(float ax, float ay);
+};
+
+struct vec3 {
+  float x;
+  float y;
+  float z;
+
+  vec3();
+  explicit vec3(float vert[]);
+  vec3(float ax, float ay, float az);
+};
 
 void normalizeVector(const vec3 *v);
