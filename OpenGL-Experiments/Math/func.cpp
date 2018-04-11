886b47a8962e1e760e4e22a1495a683b13b8dcee
Derek King <king.dm49@gmail.com>
2018-04-12 01:49:16 +1000

    Moved PI into .h

--- OpenGL-Experiments/Math/func.cpp
+++ OpenGL-Experiments/Math/func.cpp
@@ -1,5 +1,3 @@
-#define PI 3.14159265359f
-
 float degrees(float radians) {
     return radians * (180.0f/PI);
 }
--- OpenGL-Experiments/Math/func.h
+++ OpenGL-Experiments/Math/func.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#define PI 3.14159265359f
+
 float degrees(float radians);
 float radians(float degrees);
