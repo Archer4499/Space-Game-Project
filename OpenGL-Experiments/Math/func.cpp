f0b5fc8b8962df6c055ff55c9a45d1d667125e3d
Derek King <king.dm49@gmail.com>
2018-05-08 07:35:55 +1000

    Add lerp and clamp

--- OpenGL-Experiments/Math/func.cpp
+++ OpenGL-Experiments/Math/func.cpp
@@ -6,3 +6,16 @@ float degrees(float radians) {
 float radians(float degrees) {
     return degrees * (PI/180.0f);
 }
+
+float lerp(float a, float b, float value) {
+    return a + (b - a) * value;
+}
+
+float clamp(float value, float min, float max) {
+    if (value > max)
+        return max;
+    else if (value < min)
+        return min;
+    else
+        return value;
+}
--- OpenGL-Experiments/Math/func.h
+++ OpenGL-Experiments/Math/func.h
@@ -4,3 +4,7 @@
 
 float degrees(float radians);
 float radians(float degrees);
+
+float lerp(float a, float b, float value);
+
+float clamp(float value, float min, float max);
