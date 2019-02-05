a71c0a26569790eccef8adcb08edfdca943877a3
Derek King <king.dm49@gmail.com>
2019-02-05 19:40:25 +1100

    Add max and min

--- Space-Game-Project/Math/func.cpp
+++ Space-Game-Project/Math/func.cpp
@@ -1,10 +1,25 @@
 #include "func.h"
 
+int max(int a, int b) {
+    return (a<b)?b:a;
+}
+int min(int a, int b) {
+    return (a>b)?b:a;
+}
+
+
 float degrees(float radians) {
     return radians * (180.0f/PI);
 }
 float radians(float degrees) {
     return degrees * (PI/180.0f);
+}
+
+float max(float a, float b) {
+    return (a<b)?b:a;
+}
+float min(float a, float b) {
+    return (a>b)?b:a;
 }
 
 float lerp(float a, float b, float value) {
@@ -18,4 +33,12 @@ float clamp(float value, float min, float max) {
         return min;
     else
         return value;
+}
+
+
+double max(double a, double b) {
+    return (a<b)?b:a;
+}
+double min(double a, double b) {
+    return (a>b)?b:a;
 }
--- Space-Game-Project/Math/func.h
+++ Space-Game-Project/Math/func.h
@@ -2,9 +2,18 @@
 
 #define PI 3.14159265359f
 
+int max(int a, int b);
+int min(int a, int b);
+
 float degrees(float radians);
 float radians(float degrees);
+
+float max(float a, float b);
+float min(float a, float b);
 
 float lerp(float a, float b, float value);
 
 float clamp(float value, float min, float max);
+
+double max(double a, double b);
+double min(double a, double b);
