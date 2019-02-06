8724918916c9bdcbcfcb7427505df1dc54a49d1c
Derek King <king.dm49@gmail.com>
2019-02-07 01:43:04 +1100

    Clarify comment

--- Space-Game-Project/stringUtils.h
+++ Space-Game-Project/stringUtils.h
@@ -5,10 +5,10 @@
 // Starts at i and sets i to the index of the next non-white-space(Except \n) character in str after the index i
 void eatSpaces(const std::string &str, size_t &i, bool newLines = false);
 
-// Returns the substring until the next white-space char, stops at \n chars even with eatSpace
+// Returns the substring until the next white-space char, stops before \n chars even with shouldEatSpaces
 std::string stringUntilSpace(const std::string &str, size_t &i, const bool shouldEatSpaces = true);
 
 // Starts at i and sets i to the index of the char after the next \n
 //   if there is a comment before any other text otherwise doesn't change i
 // Returns true if a comment was found
-bool skipComments(const std::string &str, size_t &i) ;
+bool skipComments(const std::string &str, size_t &i);
